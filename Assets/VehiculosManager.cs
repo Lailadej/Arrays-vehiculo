2f706de091d548ec7069996b40e8ff99a351cb1b
DEJTIAR LAILA <48313454@ort>
2024-06-03 09:04:05 -0300

    Arrays vehiculo cantidad de ruedas

---
+++ Assets/VehiculosManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehiculosManager : MonoBehaviour
+{
+    public Vehiculo vehiculoScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int cantRuedasVehiculos = vehiculoScript.cantRuedas;
+
+        Debug.Log("Cant ruedas: " + cantRuedasVehiculos);
+        if (cantRuedasVehiculos < 6 )
+        {
+            vehiculoScript.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
